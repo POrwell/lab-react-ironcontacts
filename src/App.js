0621481e9dfa1585e6367986019ade56b259968e
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-11-15 15:41:16 +0100

    Iteration1 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+import React, { useState } from "react";
+const reducedContacts = contacts.slice(0, 5)
 
 function App() {
+  console.log(reducedContacts)
+ const [contacts, setContacts] = useState(reducedContacts)
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+<table>
+  <thead>
+    <tr>
+      <th>Picture</th>
+      <th>Name</th>
+      <th>Popularity</th>
+    </tr>
+  </thead>
+  <tbody>
+  {contacts.map((celeb) => 
+  <tr key={celeb.id}>
+  <td><img src={celeb.pictureUrl}/></td>
+  <td>{celeb.name}</td>
+  <td>{celeb.popularity}</td>
+</tr>
+  )}
+  </tbody>
+</table>
     </div>
   );
 }
